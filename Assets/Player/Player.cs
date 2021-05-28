4f3a4b1c11c71f6a2181712cf4b6eabab836a4d0
Brandon Lee <brandon.sangmin.lee@gmail.com>
2021-05-28 12:27:44 -0500

    created shotgun sprites (png)

--- Assets/Player/Player.cs
+++ Assets/Player/Player.cs
@@ -6,6 +6,7 @@ public class Player : MonoBehaviour
 {
     public int health = 100;
     public int equippedDropIndex = 0;
+    public bool weaponActive = false;
 
     Rigidbody2D m_Rigidbody;
     SpriteRenderer mySpriteRenderer;
@@ -24,14 +25,23 @@ public class Player : MonoBehaviour
     void Update()
     {
         if (Input.GetKey(KeyCode.RightArrow)){
+            weaponActive = false;
             m_Rigidbody.velocity = transform.right * m_Thrust;
             mySpriteRenderer.flipX = false;
         } else if (Input.GetKey(KeyCode.LeftArrow)){
+            weaponActive = false;
             m_Rigidbody.velocity = -transform.right * m_Thrust;
             mySpriteRenderer.flipX = true;
         } else {
+            if (Input.GetKey(KeyCode.Space)) {
+                weaponActive = true;
+            } else {
+                weaponActive = false;
+            }
             m_Rigidbody.velocity *= 0.95f;
         }
         anim.SetFloat("speed", Mathf.Abs(m_Rigidbody.velocity.x));
+        anim.SetInteger("equip index", equippedDropIndex);
+        anim.SetBool("active", weaponActive);
     }
 }
